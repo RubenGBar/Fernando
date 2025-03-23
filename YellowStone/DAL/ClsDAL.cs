10407c6f2ff6d89e6d0fe0df02433553f4df6357
RubenGBar <ruben.garcia@iesnervion.es>
2025-03-23 20:59:46 +0100

    yellowstone terminado?

--- YellowStone/DAL/ClsDAL.cs
+++ YellowStone/DAL/ClsDAL.cs
@@ -10,7 +10,61 @@ namespace DAL
         private List<ClsRaza> listadoRazasDAL;
         #endregion
 
+        #region Funciones
+        private static List<ClsCaballo> obtenerListadoCompletoCaballoDAL()
+        {
+            return new List<ClsCaballo>
+            {
+                new ClsCaballo(1, "Blue Note", 0),
+                new ClsCaballo(2, "Star", 0),
+                new ClsCaballo(3, "Brandy", 0),
+                new ClsCaballo(4, "Lucky", 0),
+                new ClsCaballo(5, "Apollo", 0),
+                new ClsCaballo(6, "Scout", 0),
+                new ClsCaballo(7, "Dakota", 0),
+                new ClsCaballo(8, "Cash", 0)
+            };
+        }
 
+        private static List<ClsRaza> obtenerListadoCompletoRazaDAL()
+        {
+            return new List<ClsRaza>
+            {
+                new ClsRaza(1, "Mustang"),
+                new ClsRaza(2, "Paint Horse"),
+                new ClsRaza(3, "Rocky Mountain"),
+                new ClsRaza(4, "Appaloosa"),
+                new ClsRaza(5, "Pinto"),
+                new ClsRaza(6, "Cortador")
+            };
+        }
+
+        public static List<ClsCaballo> obtenerListadoCaballoDAL()
+        {
+            return obtenerListadoCompletoCaballoDAL();
+        }
+        public static List<ClsRaza> obtenerListadoRazaDAL()
+        {
+            return obtenerListadoCompletoRazaDAL();
+        }
+
+        public static int actualizarRazaCaballoDAL(int idCaballo, int idRaza)
+        {
+            int numeroFilasAfectadas = 0;
+            List<ClsCaballo> listadoCaballosDAL = obtenerListadoCompletoCaballoDAL();
+
+            foreach (var caballo in listadoCaballosDAL)
+            {
+                if (caballo.IdCaballo > 0 && caballo.IdCaballo == idCaballo)
+                {
+                    caballo.IdRaza = idRaza;
+                    numeroFilasAfectadas++;
+                }
+            }
+
+            return numeroFilasAfectadas;
+        }
+        #endregion
 
     }
 }
---
+++ YellowStone/MAUI/Models/VM.cs
@@ -0,0 +1,18 @@
+﻿using ENT;
+using BL;
+using System.Collections.ObjectModel;
+using MAUI.Models.Model;
+
+namespace MAUI.Models
+{
+    internal class VM
+    {
+
+        #region Propiedades
+        public ObservableCollection<ClsCaballoConListadoRazas> listadoDeCaballosConRazas { get; }
+        #endregion
+
+        // Commands
+
+    }
+}
